9b68e562426bf61e1686448ef1f2b980364f8335
Ashlin Harris <ashlin_harris@brown.edu>
2022-08-19 12:18:43 -0400

    Spin through string

--- src/Spinners.jl
+++ src/Spinners.jl
@@ -2,6 +2,13 @@ module Spinners
 
 export spinner
 
-spinner() = println("X")
+	function spinner(string)
+		for i in 1:length(string)
+			print(string[i])
+			sleep(0.5)
+			print("\b")
+		end
+		println()
+	end
 
 end # module Spinners

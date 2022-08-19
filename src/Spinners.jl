9d8e2898a4287c4734e7986f373262dade871333
Ashlin Harris <ashlin_harris@brown.edu>
2022-08-19 13:12:49 -0400

    Add function argument for time

--- src/Spinners.jl
+++ src/Spinners.jl
@@ -2,18 +2,14 @@ module Spinners
 
 export spinner
 
-	function spinner(string)
+	function spinner(string, time)
 
-		#a = sleep(7)
-		#t = Task(a)
 		t = @async sleep(4)
-		#schedule(t)
-		#yield()
 
 		i = 0
 		while !istaskdone(t)
 			print(string[ ( i % length(string)  ) + 1 ])
-			sleep(0.5)
+			sleep(time)
 			print("\b")
 			i = i + 1
 		end

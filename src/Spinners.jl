4df26275894e432f1c0ec4e0b79a566767f6511a
Ashlin Harris <ashlin_harris@brown.edu>
2022-08-19 14:01:01 -0400

    Combine print statements to avoid collision

--- src/Spinners.jl
+++ src/Spinners.jl
@@ -2,29 +2,31 @@ module Spinners
 
 export spinner
 
-	function spinner(
-		string::Union{String, Nothing}=nothing,
-		time::Union{AbstractFloat, Nothing}=nothing,
-		)
+function spinner(
+	t::Union{Task, Nothing}=nothing,
+	string::Union{String, Nothing}=nothing,
+	time::Union{AbstractFloat, Nothing}=nothing,
+	)
 
+	# Assign missing arguments
+	if isnothing(t)
+		t = @async sleep(3)
+	end
 	if isnothing(string)
 		string = "\\|/-"
 	end
-
 	if isnothing(time)
 		time = 0.1
 	end
 
-		t = @async sleep(4)
-
-		i = 0
-		while !istaskdone(t)
-			print(string[ ( i % length(string)  ) + 1 ])
-			sleep(time)
-			print("\b")
-			i = i + 1
-		end
-		println()
+	# Spinner
+	i = 0
+	while !istaskdone(t)
+		print("\b", string[ ( i % length(string)  ) + 1 ])
+		sleep(time)
+		i = i + 1
 	end
+	println()
+end
 
 end # module Spinners

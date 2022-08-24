9f5533da2e074d91c076a5bfa1c470cb6ca5958f
AshlinHarris <ashlin_harris@brown.edu>
2022-08-24 19:51:56 -0400

    Add g()

--- dist.jl
+++ dist.jl
@@ -6,21 +6,25 @@ using Distributed
 
 #df = DataFrame(a=rand(1000), b=rand(1:10, 1000))
 
-@everywhere function f(c, d)
-	#sleep(0.1)
-	#s = 0
-	#for i in 10_000_000:10_000_001
-	#	s += BigInt(999)^i % 17
-	#end
-	#sleep(0.1)
+@everywhere function f()
+	sleep(0.1)
+	s = 0
+	for i in 10_000_000:10_000_001
+		s += BigInt(999)^i % 17
+	end
+	sleep(0.1)
 
-	#return s
+	return s
+end
 
+@everywhere function g(c, d)
 	return (c, d)
 end
 
 r = Future()
-@async put!(r, remotecall_fetch(f, w, 2, 3))
+@async put!(r, remotecall_fetch(f, w))
+#@async put!(r, remotecall_fetch(g, w, 2, 3))
+#
 #@async put!(r, remotecall_fetch(pivot, w, df))
 #@async put!(r, remotecall_fetch(, w, "test"))
 

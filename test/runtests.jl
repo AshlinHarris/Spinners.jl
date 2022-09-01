b181120ca27c164ca6e877f3500e1abbafa6efab
Ashlin Harris <ashlin_harris@brown.edu>
2022-09-01 17:15:34 -0400

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,9 @@ using Random
 using Spinners
 using Test
 
-@test true
-
-#=
-
 # Look at this
 function output_test(command::Expr, expected::String)
-	Random.seed!(37)
+	Random.seed!(37) # Make this depend on the version?
 	os = stdout;
 	(rd, wr) = redirect_stdout();
 	eval(command)
@@ -19,6 +15,120 @@ function output_test(command::Expr, expected::String)
 end
 
 # Don't look at this
+
+output_test(
+	:( @spinner ),
+	"\e[?25l◒\b◐\b◓\b◑\b◒\b◐\b◓\b◑\b◒\b◐\b◓\b◑\b◒\b◐\b◓\b◑\b◒\b◐\b◓\b◑\b◒\b◐\b\b\b   \b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner sleep(1) ),
+	"\e[?25l◒\b◐\b◓\b◑\b◒\b◐\b\b\b   \b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner "1234567890" sleep(0.5) ),
+	"\e[?25l1\b2\b \b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner "abcdefg" sleep(1) ),
+	"\e[?25la\bb\bc\bd\be\bf\b \b\e[0J\e[?25h"
+)
+
+# hexagrams
+output_test(
+	:( @spinner "䷀䷁䷂䷃䷄䷅䷆䷇䷈䷉䷊䷋䷌䷍䷎䷏䷐䷑䷒䷓䷔䷕䷖䷗䷘䷙䷚䷛䷜䷝䷞䷟䷠䷡䷢䷣䷤䷥䷦䷧䷨䷩䷪䷫䷬䷭䷮䷯䷰䷱䷲䷳䷴䷵䷶䷷䷸䷹䷺䷻䷼䷽䷾䷿" ),
+	"\e[?25l䷀\b䷁\b䷂\b䷃\b䷄\b䷅\b䷆\b䷇\b䷈\b䷉\b䷊\b䷋\b䷌\b䷍\b䷎\b䷏\b䷐\b䷑\b䷒\b䷓\b䷔\b\b\b   \b\b\b\e[0J\e[?25h"
+)
+
+# tetragrams
+output_test(
+	:( @spinner "𝌆 𝌇 𝌈 𝌉 𝌊 𝌋 𝌌 𝌍 𝌎 𝌏 𝌐 𝌑 𝌒 𝌓 𝌔 𝌕 𝌖 𝌗 𝌘 𝌙 𝌚 𝌛 𝌜 𝌝 𝌞 𝌟 𝌠 𝌡 𝌢 𝌣 𝌤 𝌥 𝌦 𝌧 𝌨 𝌩 𝌪 𝌫 𝌬 𝌭 𝌮 𝌯 𝌰 𝌱 𝌲 𝌳 𝌴 𝌵 𝌶 𝌷 𝌸 𝌹 𝌺 𝌻 𝌼 𝌽 𝌾
+𝌿 𝍀 𝍁 𝍂 𝍃 𝍄 𝍅 𝍆 𝍇 𝍈 𝍉 𝍊 𝍋 𝍌 𝍍 𝍎 𝍏 𝍐 𝍑 𝍒 𝍓 𝍔 𝍕 𝍖 " sleep(0.5) ),
+	"\e[?25l𝌆\b\b \b\b\b\b    \b\b\b\b\e[0J\e[?25h"
+)
+
+# trigrams
+output_test(
+	:( @spinner "☰☱☲☳☴☵☶☷" sleep(0.5) ),
+	"\e[?25l☰\b☱\b\b\b   \b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner :arrow ),
+	"\e[?25l←\b↖\b↑\b↗\b→\b↘\b↓\b↙\b←\b↖\b↑\b↗\b→\b↘\b↓\b↙\b←\b↖\b↑\b↗\b→\b\b\b   \b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner :bar ),
+	"\e[?25l▁\b▂\b▃\b▄\b▅\b▆\b▇\b█\b▇\b▆\b▅\b▄\b▃\b▂\b▁\b▁\b▂\b▃\b▄\b▅\b▆\b\b\b   \b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner :blink ),
+	"\e[?25l⊙\b⊙\b⊙\b⊙\b⊙\b⊙\b⊙\b◡\b⊙\b⊙\b⊙\b⊙\b⊙\b⊙\b⊙\b◡\b⊙\b⊙\b⊙\b⊙\b⊙\b\b\b   \b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner :bounce sleep(0.1) ),
+	"\e[?25l\b\b\b\b\b\b\b\b\b         \b\b\b\b\b\b\b\b\b\e[0J\e[?25h"
+	#"\e[?25l(●    )\b\b\b\b\b\b\b( ●   )\b\b\b\b\b\b\b(  ●  )\b\b\b\b\b\b\b(   ● )\b\b\b\b\b\b\b(    ●)\b\b\b\b\b\b\b(   ● )\b\b\b\b\b\b\b(  ●  )\b\b\b\b\b\b\b( ●   )\b\b\b\b\b\b\b(●    )\b\b\b\b\b\b\b( ●   )\b\b\b\b\b\b\b(  ●  )\b\b\b\b\b\b\b(   ● )\b\b\b\b\b\b\b(    ●)\b\b\b\b\b\b\b(   ● )\b\b\b\b\b\b\b(  ●  )\b\b\b\b\b\b\b( ●   )\b\b\b\b\b\b\b(●    )\b\b\b\b\b\b\b( ●   )\b\b\b\b\b\b\b(  ●  )\b\b\b\b\b\b\b(   ● )\b\b\b\b\b\b\b(    ●)\b\b\b\b\b\b\b\b\b         \b\b\b\b\b\b\b\b\b\e[0J\e[?25h"
+	#"\e[?25l(●    )\b\b\b\b\b\b\b( ●   )\b\b\b\b\b\b\b(  ●  )\b\b\b\b\b\b\b(   ● )\b\b\b\b\b\b\b(    ●)\b\b\b\b\b\b\b(   ● )\b\b\b\b\b\b\b(  ●  )\b\b\b\b\b\b\b( ●   )\b\b\b\b\b\b\b(●    )\b\b\b\b\b\b\b( ●   )\b\b\b\b\b\b\b(  ●  )\b\b\b\b\b\b\b(   ● )\b\b\b\b\b\b\b(    ●)\b\b\b\b\b\b\b(   ● )\b\b\b\b\b\b\b(  ●  )\b\b\b\b\b\b\b( ●   )\b\b\b\b\b\b\b(●    )\b\b\b\b\b\b\b( ●   )\b\b\b\b\b\b\b(  ●  )\b\b\b\b\b\b\b(   ● )\b\b\b\b\b\b\b(    ●)\b\b\b\b\b\b\b(   ● )\b\b\b\b\b\b\b\b\b         \b\b\b\b\b\b\b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner :cards sleep(0.2) ),
+	"\e[?25l\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b                    \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner :clock sleep(0.5) ),
+	"\e[?25l🕐\b\b🕑\b\b\b\b    \b\b\b\b\e[0J\e[?25h"
+	#"\e[?25l🕐\b\b🕑\b\b🕒\b\b🕓\b\b🕔\b\b🕕\b\b🕖\b\b🕗\b\b🕘\b\b🕙\b\b🕚\b\b🕛\b\b🕐\b\b🕑\b\b🕒\b\b🕓\b\b🕔\b\b🕕\b\b🕖\b\b🕗\b\b🕘\b\b\b\b    \b\b\b\b\e[0J\e[?25h"
+	#"\e[?25l🕐\b\b🕑\b\b🕒\b\b🕓\b\b🕔\b\b🕕\b\b🕖\b\b🕗\b\b🕘\b\b🕙\b\b🕚\b\b🕛\b\b🕐\b\b🕑\b\b🕒\b\b🕓\b\b🕔\b\b🕕\b\b🕖\b\b\b\b    \b\b\b\b\e[0J\e[?25h"
+	#"\e[?25l🕐\b\b🕑\b\b🕒\b\b🕓\b\b🕔\b\b🕕\b\b🕖\b\b🕗\b\b🕘\b\b🕙\b\b🕚\b\b🕛\b\b🕐\b\b🕑\b\b🕒\b\b🕓\b\b🕔\b\b🕕\b\b🕖\b\b🕗\b\b🕘\b\b\b\b    \b\b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner :dots sleep(0.5) ),
+	"\e[?25l⠁\b⠂\b\b\b   \b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner :loading sleep(0.5) ),
+	"\e[?25lLoading.    \b\b\b\b\b\b\b\b\b\b\b\bLoading..   \b\b\b\b\b\b\b\b\b\b\b\b            \b\b\b\b\b\b\b\b\b\b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner :moon sleep(0.5) ),
+	"\e[?25l🌑\b\b🌒\b\b\b\b    \b\b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner :pong sleep(0.5) ),
+	"\e[?25l▐⠂       ▌\b\b\b\b\b\b\b\b\b\b▐⠈       ▌\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b                \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner :shutter sleep(0.5) ),
+	"\e[?25l▉\b▊\b\b\b   \b\b\b\e[0J\e[?25h"
+)
+
+output_test(
+	:( @spinner :snail sleep(0.5) ),
+	"\e[?25l🐌        🏁\b\b\b\b\b\b\b\b\b\b\b\b🐌        🏁\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b                \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\e[0J\e[?25h"
+
+)
+
+output_test(
+	:( @spinner :a125🐌125yvg35hfddyu sleep(0.5) ),
+	"\e[?25l?\b \b \b\e[0J\e[?25h"
+)
+
+
+#=
+
 
 output_test(
 	:(t = @async Task(:); spinner(t)),

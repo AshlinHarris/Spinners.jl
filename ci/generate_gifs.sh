a88fb1d32999f28260b9599d6e66e3d84b9fa2f8
Brian Sinquin <paradisepx@gmail.com>
2022-10-23 02:23:33 +0200

    remove julia header
    The dirty way

--- ci/generate_gifs.sh
+++ ci/generate_gifs.sh
@@ -2,7 +2,11 @@
 
 # One need to have python and asciinema installed
 # One need to have rust-compiled and installed asciinema/agg tool
-
+julia --project=@. -e "using Pkg; Pkg.instantiate()"
 rm result.cast
 asciinema rec result.cast --command "julia --project=@. generate_gifs.jl"
-agg result.cast test.gif
+
+# remove julia header lines
+sed '2,6d' result.cast > result2.cast
+
+agg result2.cast test.gif

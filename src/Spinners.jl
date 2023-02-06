76f586aa0f88068f63fb127f29f13deb70ba9eab
Ashlin Harris <ashlin_harris@brown.edu>
2023-02-06 12:40:41 -0500

    Add instructions to macro docstring

--- src/Spinners.jl
+++ src/Spinners.jl
@@ -120,6 +120,20 @@ end
 
 """
 `@spinner`: Command line spinners with Unicode support
+
+Usage: `@spinner [style] [rate] [message] function`
+
+  - `style`: `String`, `Vector{String}`, or `Symbol`
+    - See `Spinners.SPINNERS` for the list of supported symbols
+  - `rate`: seconds per frame
+  - `message::String`: text displayed to the right of the spinner
+
+Examples:
+```
+	@spinner
+	@spinner :aesthetic
+	@spinner "◧◨" 0.5 sleep(5)
+```
 """
 macro spinner()
 	quote
